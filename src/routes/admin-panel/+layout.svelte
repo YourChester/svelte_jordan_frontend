dff81f72fbc21bd2485bd456e00a42cc611f599a
Artur Kamenev <artyrkamenev@gmail.com>
2025-07-23 23:27:29 +0300

    ADD: added start structure

---
+++ src/routes/admin-panel/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let { children } = $props()
+</script>
+
+<div class="admin-layout">
+	<header>Панель администратора</header>
+	<aside>
+		<ul>
+			<li>
+				<a href="admin-panel/settings/genders">Пол</a>
+			</li>
+		</ul>
+	</aside>
+	<div class="admin-layout__page__conteiner">
+		<main class="admin-layout__page">
+			{@render children()}
+		</main>
+	</div>
+</div>
